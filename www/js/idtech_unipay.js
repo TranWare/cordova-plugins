eb1814f20826ab7853661e92c3ba733dd13ff7cd
Kevin Krumwiede <kjkrum@gmail.com>
2015-03-11 14:16:22 -0700

    Changed js.

--- www/js/idtech_unipay.js
+++ www/js/idtech_unipay.js
@@ -1,9 +1,24 @@
- 'use strict';
+'use strict';
 
    var IDTech = ( typeof IDTech === 'undefined' ? {} : IDTech );
    var cordova = window.cordova || window.Cordova,
-       IDTech.fnord = function(callback, error) {
-       cordova.exec(callback, error, 'com.tranware.UniPay', 'fnord', []);
+
+   fail = function(error) {
+       console.log('Error running your request: ' + error);
    };
+   
+       IDTech.fnord = function(callback, error) {
+
+       var success = function(fnord) {
+       	console.log(fnord)
+       	callback(fnord);
+       };
+       var fail_handler =  fail;
+       
+       cordova.exec(success, fail_handler, 'com.tranware.UniPay', 'fnord', []);
+
+     //  cordova.exec(callback, error, 'com.tranware.UniPay', 'fnord', []);
+   };
+
 
    module.exports = IDTech;

f6ec69d2d042560d4735fa65ec12d13052f38acf
Kevin Krumwiede <kjkrum@gmail.com>
2015-03-11 13:53:01 -0700

    js tweaks

--- www/js/idtech_unipay.js
+++ www/js/idtech_unipay.js
@@ -1,22 +1,9 @@
  'use strict';
- 
-    var IDTech = ( typeof IDTech === 'undefined' ? {} : IDTech );
-    var cordova = window.cordova || window.Cordova,
-        fail = function(error) {
-            console.log('Error running your request: ' + error);
-        };
- 
-        IDTech.fnord = function(callback, error) {
-        $('.error').html('Making cordova.exec call to fnord', []);
-        var success = function(fnord) {
-           	$("#result").html(fnord);
-               callback(fnord);
-           };
-        var fail_handler = error || fail;
 
-        cordova.exec(success, fail_handler, 'com.tranware.UniPay', 'fnord', []);
-        $('.error').html('Made cordova.exec call to fnord');
-    };
+   var IDTech = ( typeof IDTech === 'undefined' ? {} : IDTech );
+   var cordova = window.cordova || window.Cordova,
+       IDTech.fnord = function(callback, error) {
+       cordova.exec(callback, error, 'com.tranware.UniPay', 'fnord', []);
+   };
 
-
-    module.exports = IDTech;
+   module.exports = IDTech;

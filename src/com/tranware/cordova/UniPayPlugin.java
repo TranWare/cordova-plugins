f072fcc0de4a1e795b24d810c7a373da4ed3aa1c
Kevin Krumwiede <kjkrum@gmail.com>
2015-03-16 16:02:34 -0700

    First swing at robust swiping.

--- src/com/tranware/cordova/UniPayPlugin.java
+++ src/com/tranware/cordova/UniPayPlugin.java
@@ -6,6 +6,7 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -16,12 +17,26 @@ import android.util.Log;
 import com.idtechproducts.unipay.StructConfigParameters;
 import com.idtechproducts.unipay.UniPayReader;
 
+/**
+ * Cordova plugin wrapper for the UniPay SDK.  Attempts to work around the
+ * SDK's triple whammy of poor design, incomplete and sometimes incorrect
+ * documentation, and numerous bugs.  Some of the bugs are worked around in
+ * such a way that if they're ever fixed, this wrapper will stop working.
+ *
+ * @author Kevin Krumwiede
+ */
 public class UniPayPlugin extends CordovaPlugin {
 	private static final String TAG = UniPayPlugin.class.getSimpleName();
 	
 	private static final String ACTION_DETECT_READER = "ACTION_DETECT_READER";
 	private static final String RESULT_DETECTED = "RESULT_DETECTED";
 	private static final String ERROR_NOT_DETECTED = "ERROR_NOT_DETECTED";
+	
+	private static final String ACTION_GET_SWIPE = "ACTION_GET_SWIPE";
+	private static final String ERROR_TIMEOUT = "ERROR_TIMEOUT";
+	private static final String ERROR_NO_TRACK_2 = "ERROR_NO_TRACK_2";
+	private static final String ERROR_UNKNOWN = "ERROR_UNKNOWN";
+	
 	
 	private UniPayReader mReader;	
 	private BroadcastReceiver mHeadsetReceiver;
@@ -78,6 +93,15 @@ public class UniPayPlugin extends CordovaPlugin {
 			}			
 			return true;
 		}
+		else if(ACTION_GET_SWIPE.equals(action)) {
+			if(mDetected) {
+				mReader.sendCommandEnableSwipingMSRCard();
+			}
+			else {
+				error(ERROR_NOT_DETECTED);
+			}
+			return true;
+		}
 		
 		return false;
 	}
@@ -94,6 +118,9 @@ public class UniPayPlugin extends CordovaPlugin {
 		
 		@Override
 		public void onReceiveMsgTimeout(String message) {
+			/* This is never actually called for a swipe timeout, only when
+			 * handshaking fails.
+			 */
 			Log.d(TAG, "onReceiveMsgTimeout(\"" + message + "\")");
 			destroyReader();
 			error(ERROR_NOT_DETECTED);
@@ -114,25 +141,110 @@ public class UniPayPlugin extends CordovaPlugin {
 		@Override
 		public void onReceiveMsgDisconnected() {
 			Log.d(TAG, "onReceiveMsgDisconnected()");
-			destroyReader();
-			
 			/* This ensures that Cordova gets a callback if some action is
 			 * interrupted by the device being unplugged.  This comes into
-			 * play if the app tries to handshake with headphones and the user
-			 * unplugs them to save their ears.  Probably also if the user
-			 * unplugs the reader during a swipe attempt.
+			 * play if the reader tries to handshake with headphones and the
+			 * user unplugs them to save their ears.  Probably also if the
+			 * user unplugs the reader during a swipe attempt.
 			 */
 			error(ERROR_NOT_DETECTED);
-			
+			destroyReader();			
 			mDetected = false;
+		}
+		
+		@Override
+		public void onReceiveMsgCommandResult(int command, byte[] response) {
+			/* Example of response bytes indicating success: 2 1 0 6 6 6 3
+			 * There are more conditions in the sample code, but this seems to
+			 * cover it for our purposes.  See UniPayDemo line 2080.  (!!!)
+			 */
+			if(command == cmdEnableSwipingMSRCard) {
+				if(response.length > 3 && response[3] == 6) {					
+					mReader.startSwipeCard();
+				}
+				else {
+					StringBuilder sb = new StringBuilder();
+					sb.append("unexpected result bytes (decimal):");
+					for(byte b : response) {
+						sb.append(' ').append(Integer.toString(b));
+					}
+					Log.w(TAG, sb.toString());
+					error(ERROR_UNKNOWN);
+				}
+			}
+			else if(command == cmdCancelSwipingMSRCard) {
+				// not unexpected
+			}
+			else {
+				Log.w(TAG, "unexpected command result: " + command);
+			}
+		}
+		
+		@Override
+		public void onReceiveMsgCardData(byte flags, byte[] data) {
+			if(flags == 0) {
+				Log.d(TAG, "received swipe");
+				final String trackData = new String(data);				
+				Track2Matcher matcher = new Track2Matcher();
+				if(matcher.find(trackData)) {
+					JSONObject result = new JSONObject();
+					try {
+						result.put("card", matcher.getCard());
+						result.put("exp", matcher.getExpMMYY());
+						result.put("raw", trackData);
+						success(result);
+					} catch (JSONException e) {
+						Log.w(TAG, "\"impossible\" exception", e);
+						error(ERROR_UNKNOWN);
+					}
+				}
+				else {
+					error(ERROR_NO_TRACK_2);
+				}
+			}
+			else {
+				Log.d(TAG, "received garbage swipe - probably bogus timeout");				
+				error(ERROR_TIMEOUT);
+			}
 		}
 
 	}
 	
 	private void initReader() {
+		/* If detection ever fails, the reader can get into an unrecoverable
+		 * state that requires force closing the app to make it work again.
+		 * To ensure reliability, we destroy and recreate the reader each time
+		 * it's used.
+		 */		
 		destroyReader();
 		mReader = new UniPayReader(new ReaderCallback(), cordova.getActivity());
-
+		/* Force the reader to use the profile called template_9600_1.  This
+		 * is far simpler than hassling with config files, and actually
+		 * supports some devices that are not supported in the config file.
+		 * Every device we've tested on which autoconfig works at all is
+		 * configured with this template.
+		 * 
+		 * <template_9600_1>
+		 *    <support_status val=""> </support_status>
+		 *    <directionOutputWave>1</directionOutputWave>
+		 *    <InputFreq>48000</InputFreq>
+		 *    <OutputFreq>48000</OutputFreq>
+		 *    <RecBuffSize>8192</RecBuffSize>
+		 *    <ReadRecBuffSize>163840</ReadRecBuffSize>
+		 *    <WaveDirct>1</WaveDirct>
+		 *    <_Low>-10000</_Low>
+		 *    <_High>10000</_High>
+		 *    <__Low>-2000</__Low>
+		 *    <__High>2000</__High>
+		 *    <highThreshold>4000</highThreshold>
+		 *    <lowThreshold>-4000</lowThreshold>
+		 *    <device_Apm_Base>25000</device_Apm_Base>
+		 *    <min>2</min>
+		 *    <max>8</max>
+		 *    <baudRate>9600</baudRate>
+		 *    <preAmbleFactor>2</preAmbleFactor>
+		 * </template_9600_1>
+		 */		
 		StructConfigParameters config = new StructConfigParameters();
 		config.setDirectionOutputWave((short) 1);
 		config.setFrequenceInput(48000);
@@ -150,20 +262,45 @@ public class UniPayPlugin extends CordovaPlugin {
 		config.setMin((short) 2);
 		config.setMax((short) 8);
 		config.setBaudRate(9600);
-		config.setPreAmbleFactor((short) 2);	    	
+		config.setPreAmbleFactor((short) 2);
+		/* There is no documentation for StructConfigParameters, and these
+		 * values are not in the config file.  I discovered the expected
+		 * values by instrumenting the demo app to log them after running
+		 * autoconfig.
+		 */
 		config.setShuttleChannel((byte) 48);
 		config.setForceHeadsetPlug((short) 0);	        	
 		config.setUseVoiceRecognition((short) 1);	        	
 		config.setVolumeLevelAdjust((short) 0);	
-		
+		/* This method is also completely undocumented.  It has the side
+		 * effect of trying to handshake with the reader, and it always logs a
+		 * warning because the SDK doesn't know the reader is connected until
+		 * we've called registerListen.
+		 */
 		mReader.connectWithProfile(config);
-		// this should result in callback to onReceiveMsgConnected or onReceiveMsgTimeout
+		/* We never receive a connected callback if we call this before
+		 * connectWithProfile.  (Oddly enough, the disconnected callback
+		 * always works.)  Once it's getting callbacks, the reader will
+		 * attempt to handshake with anything that's plugged in.  This will
+		 * result in a callback to onReceiveMsgConnected or
+		 * onReceiveMsgTimeout.  (But see the comment in
+		 * onReceiveMsgDisconnected.)
+		 */
 		mReader.registerListen();
 	}
 	
 	private void destroyReader() {
 		if(mReader != null) {
+			/* Calling this when the reader is not listening seems to have no
+			 * ill effect.
+			 */
 			mReader.unregisterListen();
+			/* The docs say to call this when the reader is disconnected, but
+			 * doing so causes it to never recognize when it is connected
+			 * again.  The demo app never calls this.  Calling it here ensures
+			 * that the record thread is stopped.  Calling it when the record
+			 * thread is not running seems to have no ill effect.
+			 */
 			mReader.release();
 			mReader = null;
 		}
@@ -174,12 +311,31 @@ public class UniPayPlugin extends CordovaPlugin {
 			mCordovaCallback.success(message);
 			mCordovaCallback = null;
 		}
+		else {
+			// shouldn't happen, but we'd like to know if it does
+			Log.w(TAG, "success not delivered - callback was null");
+		}
+	}
+	
+	private void success(JSONObject result) {
+		if(mCordovaCallback != null) {
+			mCordovaCallback.success(result);
+			mCordovaCallback = null;
+		}
+		else {
+			// shouldn't happen, but we'd like to know if it does
+			Log.w(TAG, "success not delivered - callback was null");
+		}
 	}
 	
 	private void error(String message) {
 		if(mCordovaCallback != null) {
 			mCordovaCallback.error(message);
 			mCordovaCallback = null;
+		}
+		else {
+			// shouldn't happen, but we'd like to know if it does
+			Log.w(TAG, "error not delivered - callback was null");
 		}
 	}
 	
